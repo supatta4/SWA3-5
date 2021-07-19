9c893bb1f1f0452aa0787197a7957934a66cb3d5
Supatta <supatta435@gmail.com>
2021-07-19 16:03:46 +0700

    Initial commit

---
+++ src/com/journaldev/composition/Person.java
@@ -0,0 +1,17 @@
+
+package com.journaldev.composition;
+
+public class Person {
+
+    //composition has-a relationship
+    private Job job;
+
+    public Person(){
+        this.job=new Job();
+        job.setSalary(1000L);
+    }
+    public long getSalary() {
+        return job.getSalary();
+    }
+
+}
---
+++ src/com/journaldev/composition/TestPerson.java
@@ -0,0 +1,10 @@
+package com.journaldev.composition;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+    }
+
+}
